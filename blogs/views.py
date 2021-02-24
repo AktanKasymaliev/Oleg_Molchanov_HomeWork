6a85f37d839333faf805ae49af59de1c606f01c2
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-02-24 15:23:05 +0600

    5 Video

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 def posts_list(request):
-    n = ['Aktan', 'beka', 'altai']
+    posts = Post.objects.all()
     context ={
-        'name': n
+        'posts': posts
     }
     return render(request, 'blogs/index.html', context)
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blogs/post_detail.html', {'post':post})
